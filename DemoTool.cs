1d5f15c2696cc4ad4604035cdde1a49616c9c535
Tibart Visser <tibart.visser@9altitudes.com>
2026-02-09 14:40:39 +0100

    Add basic MCP server config and tools

---
+++ DemoTool.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using Microsoft.Extensions.AI;
+using ModelContextProtocol.Server;
+
+namespace NineAFirstMCPServer;
+
+[McpServerToolType]
+public static class DemoTool
+{
+    [McpServerTool, Description("Tell MCP server name to the client.")]
+    public static string MyName() => $"My name is '{nameof(NineAFirstMCPServer)} von NineAltitudes'.";
+
+    [McpServerTool, Description("Converts the message to camelCase.")]
+    public static string CamelCase(string message)
+    {
+        var words = message.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        if (words.Length == 0) return string.Empty;
+
+        var result = words[0].ToLower();
+        for (int i = 1; i < words.Length; i++)
+        {
+            if (words[i].Length > 0)
+                result += char.ToUpper(words[i][0]) + words[i][1..].ToLower();
+        }
+        return result;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(options =>
+{
+    // stdout is reserved for MCP communication, divert logs to stderr
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+// 
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();  // Scan assembly for classes marked with [McpServerToolType] attribute 
+    
+await builder.Build().RunAsync();
